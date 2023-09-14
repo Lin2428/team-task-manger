53c630be4596503aca52ecd8e9eaf14ccb3a02fc
Lin Daily <lindailydiaba@gmail.com>
2023-09-14 10:26:05 +0200

    refactor(view): centre les formulaires de connexion et dinscription

--- templates/Users/add.php
+++ templates/Users/add.php
@@ -6,22 +6,24 @@
  */
 ?>
 
-<div class="users-form-content">
-    <?= $this->Form->create($user) ?>
- 
-    <h1 class="connect-title">Créer un <span class="font-bold">Compte</span></h1>
+<div class="center-add-user">
+    <div class="users-form-content">
+        <?= $this->Form->create($user) ?>
+
+        <h1 class="connect-title">Créer un <span class="font-bold">Compte</span></h1>
         <?php
         echo $this->Form->control('name', ['label' => 'Noms', 'required' => true]);
         echo $this->Form->control('phone', ['required' => true]);
         echo $this->Form->control('email', ['required' => true]);
         echo $this->Form->control('password', ['required' => true]);
         ?>
-    <button type="submit" class="btn btn-primary md:w-[100%] w-[250px]">Créer le compte</button>
-    <?= $this->Form->end() ?>
-</div>
+        <button type="submit" class="btn btn-primary md:w-[100%] w-[250px]">Créer le compte</button>
+        <?= $this->Form->end() ?>
+    </div>
 
-<div class="flex items-center justify-center my-10">
+    <div class="flex items-center justify-center my-10">
         <div class="md:w-[100%] md:max-w-[10rem] h-[2px] bg-gray-300"></div>
-            <a href="/users/" class="mx-3 text-primary font-sans">Ou se connecter</a>
+        <a href="/users/" class="mx-3 text-primary font-sans">Ou se connecter</a>
         <div class=" md:w-[100%] md:max-w-[10rem] h-[2px] bg-gray-300"></div>
+    </div>
 </div>
