b424fae22e53f078b1f43bdd8bbc815a678d19bc
Lin Daily <lindailydiaba@gmail.com>
2023-09-20 16:36:34 +0200

    fix(view): ajoute les liens sur les bouton du projet

--- templates/Projects/view.php
+++ templates/Projects/view.php
@@ -9,14 +9,14 @@
 <div class="mx-8 py-3">
     <h2 class="text-3xl font-[500] border-b-[0.05px] pb-4 mb-1">Tableau de bord du projet <?= $project->name ?></h2>
     <div class="flex space-x-2 items-center mb-3 pb-1 border-b">
-    <a href="/tasks/index/<?= $project->id ?>" data-tooltip-target="tooltip-taches" class="hover:bg-gray-300 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center" type="button">
+    <button data-tooltip-target="tooltip-taches" class="hover:bg-gray-300 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center" type="button">
            <i class="bi bi-list-task text-lg"></i>
-        </a>
+        </button>
         <div id="tooltip-taches" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
             Ouvrir la liste des tâches
             <div class="tooltip-arrow" data-popper-arrow></div>
         </div>
-        <a href="/projectUsers/index/<?= $project->id ?>" data-tooltip-target="tooltip-membres" class="hover:bg-gray-300 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center" type="button">
+        <a href="/projectUsers/index/<?= $project->id.'/?'.$project->id ?>" data-tooltip-target="tooltip-membres" class="hover:bg-gray-300 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center" type="button">
             <i class="bi bi-person text-xl"></i>
         </a>
         <div id="tooltip-membres" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
